d39a5148bab7fe9f159f730ad849c6e61f783536
lijanahmmed <lijanahmmed@gmail.com>
2025-04-17 23:55:57 +0600

    Explore api start

---
+++ json.js
@@ -0,0 +1,22 @@
+const user = {id: 1, name: 'Lijan Ahmmed', job: 'student'};
+const stringified = JSON.stringify(user);
+// console.log(user);
+// { id: 1, name: 'Lijan Ahmmed', job: 'student' }
+// console.log(stringified);
+// {"id":1,"name":"Lijan Ahmmed","job":"student"}
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'Taltola',
+        city: 'Dhaka',
+        country: 'Bangladesh'
+    },
+    products: ['laptop', 'mac', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+};
+console.log(shop)
+const shopJSON = JSON.stringify(shop)
+console.log(shopJSON)
